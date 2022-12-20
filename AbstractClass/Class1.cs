cbd5dbd31764d0ccd5d7c8c950e0f56c161c7aae
mmf09 <mmf09@YPC>
2022-12-19 22:52:08 -0800

    Add project files.

---
+++ AbstractClass/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClass
+{
+    public abstract class Person
+    {
+      
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public abstract void SayName();
+
+    }
+
+    public class Employee: Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+    }
+
+}
---
+++ AbstractClass/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee name = new Employee() { firstName = "Sample", lastName = "Student" };
+            name.SayName();
+            Console.ReadLine();
+        }
+    }
+}
